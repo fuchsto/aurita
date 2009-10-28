269c0bf8e8fb3134ce0603d39e1f24976754f656
Tobias W. H. Fuchs <twh.fuchs@gmail.com>
2009-10-29 00:38:39 +0100

    - Extended GUI::Context_Menu_Element by a button that   optionally can be displayed when hovering a context menu element.

--- lib/aurita/modules/gui/context_menu.rb
+++ lib/aurita/modules/gui/context_menu.rb
@@ -5,9 +5,18 @@ require('aurita/model')
 module Aurita
 module GUI
 
+  # Adds a context menu to a GUI element, depending on a given 
+  # Aurita::Model instance (entity). 
+  # Usage: 
+  #   
+  #    article = Article.get(234)
+  #    Context_Menu_Element.new(:entity => article) { 
+  #      HTML.div.article { article.title } 
+  #    } 
+  # 
   class Context_Menu_Element < DelegateClass(Element)
 
-    def initialize(element, *args)
+    def initialize(element, *args, &block)
       if args.at(0).kind_of? Aurita::Model then
         entity = args.at(0)
         params = args.at(1)
@@ -15,6 +24,8 @@ module GUI
         params = args.at(0)
         entity = params[:entity]
       end
+      element = yield if block_given? 
+
       params ||= {}
       type = params[:type]
 
@@ -26,9 +37,9 @@ module GUI
       if !params[:id] && entity then
         params[:id] = entity.model_name.gsub('::','__').downcase + '_' << entity.key.values.join('_')
       end
-
+      
       highlight_element_id ||= params[:id] 
-
+      
       menu_params = ''
       if entity then
         menu_params = entity.url_key
@@ -38,10 +49,22 @@ module GUI
       additional_params.each_pair { |k,v|
         menu_params << "&#{k.to_s}=#{v.to_s}"
       }
-
+      
       element.id = "#{params[:id]}" unless element.dom_id()
+      
+      if params[:show_button] then
+        element = HTML.div(:class => [ type, :context_menu_element ] ) { 
+          HTML.div(:class   => :context_menu_button, 
+                   :style   => 'display: none;', 
+                   :onclick => "Aurita.open_context_menu(); ", 
+                   :id      => "#{highlight_element_id}_wrap") { HTML.img(:src => '/aurita/images/icons/edit_button.gif') } + 
+          element 
+        } 
+      end
+      
       element.onmouseout  = "Aurita.context_menu_out(this);"
       element.onmouseover = "Aurita.context_menu_over('#{type}', '#{menu_params}', '#{highlight_element_id}');"
+      
       super(element)
     end
     

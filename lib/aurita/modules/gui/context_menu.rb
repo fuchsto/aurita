6b374407e7ce82c9efb799fa943e8be68142753d
Tobias W. H. Fuchs <twh.fuchs@gmail.com>
2009-10-29 02:07:26 +0100

    - Added options for GUI::Context_Menu_Element parameter :show_button. - Added GUI::Context_Menu_Button.

--- lib/aurita/modules/gui/context_menu.rb
+++ lib/aurita/modules/gui/context_menu.rb
@@ -5,26 +5,57 @@ require('aurita/model')
 module Aurita
 module GUI
 
+  class Context_Menu_Button < DelegateClass(Element)
+
+    def initialize(highlight_element_id)
+      element = HTML.div(:class   => :context_menu_button, 
+                         :style   => 'display: none;', 
+                         :onclick => "Aurita.open_context_menu(); ", 
+                         :id      => "#{highlight_element_id}_wrap") { 
+        HTML.img(:src => '/aurita/images/icons/edit_button.gif') 
+      }
+      super(element)
+    end
+
+  end
+
   # Adds a context menu to a GUI element, depending on a given 
   # Aurita::Model instance (entity). 
+  # When parameter :show_button is defined, an additional button 
+  # will be displayed when hovering the element, which shows the 
+  # context menu on left click. 
+  #
   # Usage: 
   #   
   #    article = Article.get(234)
   #    Context_Menu_Element.new(:entity => article) { 
   #      HTML.div.article { article.title } 
   #    } 
+  #
+  # or 
+  #
+  #    Context_Menu_Element.new(article, :show_button => true) { article.title }
+  #
+  # or
+  #  
+  #    Context_Menu_Element.new(HTML.div { article.title }, :entity => article)
   # 
   class Context_Menu_Element < DelegateClass(Element)
 
-    def initialize(element, *args, &block)
-      if args.at(0).kind_of? Aurita::Model then
-        entity = args.at(0)
-        params = args.at(1)
-      else 
-        params = args.at(0)
-        entity = params[:entity]
+    def initialize(*args, &block)
+      if args.at(0).is_a? Aurita::Model then
+        entity  = args.at(0)
+        params  = args.at(1)
+        element = yield 
+      elsif args.at(0).is_a? GUI::Element then
+        element = args.at(0)
+        params  = args.at(1)
+        entity  = params[:entity]
+      elsif args.at(0).is_a? Hash then
+        params  = args.at(0)
+        entity  = params[:entity]
+        element = yield 
       end
-      element = yield if block_given? 
 
       params ||= {}
       type = params[:type]
@@ -35,7 +66,7 @@ module GUI
       highlight_element_id ||= element.dom_id()
       result = ""
       if !params[:id] && entity then
-        params[:id] = entity.model_name.gsub('::','__').downcase + '_' << entity.key.values.join('_')
+        params[:id] = entity.dom_id
       end
       
       highlight_element_id ||= params[:id] 
@@ -51,15 +82,21 @@ module GUI
       }
       
       element.id = "#{params[:id]}" unless element.dom_id()
+
+      context_button = Context_Menu_Button.new(highlight_element_id)
       
-      if params[:show_button] then
+      if params[:show_button] == true then
         element = HTML.div(:class => [ type, :context_menu_element ] ) { 
-          HTML.div(:class   => :context_menu_button, 
-                   :style   => 'display: none;', 
-                   :onclick => "Aurita.open_context_menu(); ", 
-                   :id      => "#{highlight_element_id}_wrap") { HTML.img(:src => '/aurita/images/icons/edit_button.gif') } + 
-          element 
+          context_button + element 
         } 
+      elsif params[:show_button] == :append then
+        element << HTML.div(:class => [ type, :context_menu_element ] ) { 
+           context_button
+        } 
+      elsif params[:show_button] == :prepend then
+        element.prepend(HTML.div(:class => [ type, :context_menu_element ] ) { 
+          context_button  
+        })
       end
       
       element.onmouseout  = "Aurita.context_menu_out(this);"
